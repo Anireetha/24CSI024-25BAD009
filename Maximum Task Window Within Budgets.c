95a7aca1d4115fb45fd0a7e3eec22396363e600e
Anireetha <anireetha8@gmail.com>
2026-07-24 22:44:34 +0530

    Add files via upload

---
+++ Maximum Task Window Within Budgets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    long long P;
+    scanf("%d %lld", &N, &P);
+
+    int arr[N];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int start = 0, maxLen = 0;
+    long long currentSum = 0;
+
+    for (int end = 0; end < N; end++) {
+        currentSum += arr[end];
+
+        while (currentSum > P && start <= end) {
+            currentSum -= arr[start];
+            start++;
+        }
+
+        if (currentSum <= P) {
+            int len = end - start + 1;
+            if (len > maxLen)
+                maxLen = len;
+        }
+    }
+
+    printf("Max Length: %d\n", maxLen);
+
+    return 0;
+}
